61c2fe31c34782caaac5cefbbc4a9a535b07027a
Amit Chackroborty <149465275+amitchackroborty04@users.noreply.github.com>
2024-12-27 14:45:23 +0600

    login from done

--- microdeft_react_test/src/Pages/Login.jsx
+++ microdeft_react_test/src/Pages/Login.jsx
@@ -1,47 +1,61 @@
 import React, { useState } from "react";
 import Container from "../Components/Container";
-import axios from "axios";
 import { Link } from "react-router-dom";
+import axios from "axios";
+import { ToastContainer, toast } from "react-toastify";
 
-const Registration = () => {
-  let [name, setName] = useState("");
+const Login = () => {
   let [email, setEmail] = useState("");
   let [password, setPassword] = useState("");
+  let [data, setData] = useState("");
 
-  const handleRegister = (e) => {
-    e.preventDefault();
+  let handleLogin = () => {
     axios
-      .post("https://react-interview.crd4lc.easypanel.host/api/register", {
-        name,
+      .post("https://react-interview.crd4lc.easypanel.host/api/login", {
         email,
         password,
       })
       .then((data) => {
-        console.log(data);
+        console.log(data.data.status_message);
+        localStorage.setItem("token", data.data.data.token);
+        toast(data.data.status_message, {
+          position: "top-right",
+          autoClose: 1000,
+          hideProgressBar: false,
+          closeOnClick: false,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "light",
+        });
       })
       .catch((error) => {
         console.log(error);
       });
   };
-
   return (
     <section>
       <Container>
+        <ToastContainer
+          position="top-right"
+          autoClose={5000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick={false}
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="light"
+        />
         <div className="border rounded-xl shadow-2xl   bg-red-200 mt-[50px] w-[60%] mx-auto pb-10 ">
           <h2 className="text-2xl font-bold text-[#1b5fe8] font-serif text-center pt-7 uppercase">
             wellc<span className="text-red-600">o</span>me
           </h2>
           <h3 className="text-2xl font-bold text-[#1b5fe8] font-serif text-center pt-4 uppercase">
-            Registrat<span className="text-red-600">io</span>n here
+            Log<span className="text-red-600">in</span> here
           </h3>
-          <div className="flex justify-center pt-5">
-            <input
-              onChange={(e) => setName(e.target.value)}
-              className="w-[40%] py-3 px-4 rounded-lg outline-none "
-              type="text"
-              placeholder="Name"
-            />
-          </div>
+
           <div className="flex justify-center pt-5">
             <input
               onChange={(e) => setEmail(e.target.value)}
@@ -60,19 +74,19 @@ const Registration = () => {
           </div>
           <div className="flex justify-center pt-7 ">
             <button
-              onClick={handleRegister}
+              onClick={handleLogin}
               className="w-[40%] py-3 px-4 rounded-lg bg-[#bea6c4] hover:bg-[#e589c2] text-xl font-midium hover:font-bold font-serif text-white duration-500"
             >
-              Register{" "}
+              Login{" "}
             </button>
           </div>
           <p className="text-base text-rose-500 font-medium capitalize text-center mt-4">
             {" "}
-            do you have an accounnt ?
-            <Link to ="/login">
-            <span className="text-green-500 font-bold hover:cursor-pointer hover:underline duration-500">
-              Login
-            </span>
+            do you have no account ?
+            <Link to="/">
+              <span className="text-green-500 font-bold hover:cursor-pointer hover:underline duration-500">
+                rigistration
+              </span>
             </Link>
           </p>
         </div>
@@ -81,4 +95,4 @@ const Registration = () => {
   );
 };
 
-export default Registration;
+export default Login;
--- microdeft_react_test/src/main.jsx
+++ microdeft_react_test/src/main.jsx
@@ -3,12 +3,17 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Login from './Pages/Login.jsx'
 
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <App />,
+  },
+  {
+    path: '/login',
+    element: <Login />,
   },
  
 ])
